a79ae669e536a39d49abdc78b9a3f3af8173e262
Przemek <Przemyslaw.Kutwin@gmail.com>
2020-09-05 23:12:21 +0200

    selerium function moved to seler file

--- app.py
+++ app.py
@@ -2,26 +2,8 @@ from selenium import webdriver
 import openpyxl
 import seler
 
-def selerium():
-    browser = webdriver.Firefox()
-    browser.get("https:github.com")
-    sign_in = browser.find_element_by_link_text("Sign in")
-    sign_in.click()
-    user_name_box = browser.find_element_by_id("login_field")
-    user_name_box.send_keys(login)
-    password_box = browser.find_element_by_id("password")
-    password_box.send_keys(passw)
-    password_box.submit()
-    browser.implicitly_wait(2)
-    repoz = browser.find_element_by_css_selector("div#repos-container > ul.list-style-none > li > div > a > span[title=" +repo +"]")
-    repoz.click()
-    browser.save_screenshot("screenshot" +str(row)+".png")
-    browser.quit()
-
 workbook = openpyxl.load_workbook("config_file.xlsx")
 sheet = workbook["Config"]
-
-seler.druk()
 
 for row in range(2, sheet.max_row +1):
     login = sheet.cell(row, 1).value
@@ -29,5 +11,5 @@ for row in range(2, sheet.max_row +1):
     repo = sheet.cell(row, 3).value
     if login == None or passw == None or repo == None:
         break
-    selerium()
+    seler.selerium()
 
