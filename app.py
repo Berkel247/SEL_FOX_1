7c05d1bdec591c91ba277a33ac8ffed842f75179
Przemek <Przemyslaw.Kutwin@gmail.com>
2020-09-05 21:38:22 +0200

    load login data from excel file

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 from selenium import webdriver
-import config
-from selenium.webdriver.common.keys import Keys #importuje np F5 albo RETURN
+import openpyxl
 
-browser = webdriver.Firefox()
-browser.get("https:github.com")
+#dodaj execeptiony na brak pliku i na błedny plik
+workbook = openpyxl.load_workbook("config_file.xlsx")
+sheet = workbook["Config"]
 
-sign_in = browser.find_element_by_link_text("Sign in")
-sign_in.click()
-
-'''
-musze zrobic brancha gdzie jedna wersja kodu ma login na sztywno a druga jest w pliku
-'''
-user_name_box = browser.find_element_by_id("login_field")
-user_name_box.send_keys(config.login)
-password_box = browser.find_element_by_id("password")
-password_box.send_keys(config.passw)
-password_box.submit()
-
-browser.implicitly_wait(2)
-market = browser.find_elements_by_class_name("js-selected-navigation-item")
-market[2].click()
-browser.save_screenshot("screenshot.png")
-browser.quit()
-
+for row in range(2, sheet.max_row +1):
+    login = sheet.cell(row, 1).value
+    passw = sheet.cell(row, 2).value
+    if login == None or passw == None:
+        break
+    browser = webdriver.Firefox()
+    browser.get("https:github.com")
+    sign_in = browser.find_element_by_link_text("Sign in")
+    sign_in.click()
+    user_name_box = browser.find_element_by_id("login_field")
+    user_name_box.send_keys(login)
+    password_box = browser.find_element_by_id("password")
+    password_box.send_keys(passw)
+    password_box.submit()
+    browser.implicitly_wait(2)
+    market = browser.find_elements_by_class_name("js-selected-navigation-item")
+    market[2].click()
+    browser.save_screenshot("screenshot" +str(row)+".png")
+    browser.quit()
 
